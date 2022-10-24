d3083b482ab41a73767ea5689598348a820bdbb4
YoonyounL <yyunglee97@gmail.com>
2022-10-24 23:19:15 +0900

    [FEAT] Change to create parameters at current time and location

--- Tars/Tars/Network/APIManager.swift
+++ Tars/Tars/Network/APIManager.swift
@@ -8,13 +8,36 @@
 import Foundation
 
 class AstronomyAPIManager: NetworkService {
-    func fetchPlanets() async throws -> [Body] {
-        let parameters = ["longitude": "-122.406417", "time": "21:42:48", "from_date": "2022-10-24", "elevation": "0.0", "to_date": "2022-10-24", "latitude": "37.785834"]
+    func fetchBodies() async throws -> [Body] {
+        let parameters = try getBodiesPositionsParameters()
         let url = AstronomyURL.bodiesPosition
         let endpoint = url.getEndpoint(with: parameters)
         guard let request = endpoint.getURLRequest() else { throw  NetworkError.invalidURL}
         let data: BodiesPositionsResponse = try await getRequest(request)
         print(data.bodiesData)
         return data.bodiesData
+    }
+}
+
+extension AstronomyAPIManager {
+    func getBodiesPositionsParameters() throws -> [String: String] {
+        // Get current Date
+        var parameters: [String: String] = [:]
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let currentDate = formatter.string(from: Date()).split(separator: " ")
+        let (currentDay, currentTime) = (currentDate[0], currentDate[1])
+        parameters["from_date"] = String(currentDay)
+        parameters["to_date"] =  String(currentDay)
+        parameters["time"] =  String(currentTime)
+        
+        // Get current Location
+        let locationManager = LocationManager()
+        guard let (latitude, longtitude, elevation) = locationManager.getCurrentLocation() else { throw LocationError.currentLocationFailure}
+        parameters["latitude"] = String(latitude)
+        parameters["longitude"] = String(longtitude)
+        parameters["elevation"] = String(elevation)
+        
+        return parameters
     }
 }
