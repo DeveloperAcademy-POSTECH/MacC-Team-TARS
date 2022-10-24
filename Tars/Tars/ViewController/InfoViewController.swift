695429c187265c7a7ac38c26dca371cff99ee4bf
Ayden <kaitaro77@naver.com>
2022-10-24 23:25:02 +0900

    [CHORE] InfoViewController 파일 생성

---
+++ Tars/Tars/ViewController/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  Tars
+//
+//  Created by Ayden on 2022/10/24.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
