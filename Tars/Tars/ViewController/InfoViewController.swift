0473e77a0c18a8ac081b72d8dbab70e56864f90c
Ayden <kaitaro77@naver.com>
2022-10-25 00:15:51 +0900

    [ADD] ImageView 추가, Asset에 사진 변경

--- Tars/Tars/ViewController/InfoViewController.swift
+++ Tars/Tars/ViewController/InfoViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 class InfoViewController: UIViewController {
     lazy var planetImageView: UIImageView = {
         let planetImage = UIImageView()
-        planetImage.image = UIImage(named: <#T##String#>)
+        planetImage.image = UIImage(named: "jupiter")
+        planetImage.contentMode = .scaleAspectFill
+        return planetImage
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
+        view.addSubview(planetImageView)
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        planetImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            planetImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            planetImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40)
+            
+        ])
     }
 }
