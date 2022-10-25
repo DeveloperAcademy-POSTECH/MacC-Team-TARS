7c366728b8944c5e54825b9c1a1220303dc244a1
Ayden <kaitaro77@naver.com>
2022-10-25 22:05:31 +0900

    [FIX] SceneDelegate 메인 VC로 복구

--- Tars/Tars/SceneDelegate.swift
+++ Tars/Tars/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        window?.rootViewController = InfoViewController()
+        window?.rootViewController = UniverseViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
