bfea76605411d59ad2da78d3ceb04ca33394f74f
YoonyounL <yyunglee97@gmail.com>
2022-10-26 21:06:54 +0900

    [Fix] Fix degree to radian

---
+++ Tars/Tars/Extensions/Float+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Float+Extension.swift
+//  Tars
+//
+//  Created by 이윤영 on 2022/10/26.
+//
+
+import Foundation
+
+extension Float {
+    func degreeToRadian() -> Float {
+        return self * .pi / 180
+    }
+}
