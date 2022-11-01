c1f3d8518f8b702f0e1b87ecd8f331b05d46b33e
YoonyounL <yyunglee97@gmail.com>
2022-11-01 22:58:57 +0900

    [FEAT] Make user go to app settings when location authorization restricted or denied

--- Tars/Tars/Network/LocationManager.swift
+++ Tars/Tars/Network/LocationManager.swift
@@ -13,10 +13,6 @@ enum LocationError: Error {
     case currentLocationFailure
 }
 
-protocol LocationManagerDelegate: AnyObject, UIViewController {
-    func didUpdateUserLocation()
-}
-
 class LocationManager: NSObject, CLLocationManagerDelegate {
     static let shared = LocationManager()
     weak var delegate: LocationManagerDelegate?
@@ -51,7 +47,7 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         case .authorizedWhenInUse:
             manager.startUpdatingLocation()
         case .restricted, .denied:
-            print("authorization restricted or denied")
+            openSetting()
         case .notDetermined:
             manager.requestWhenInUseAuthorization()
         default:
@@ -78,6 +74,33 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     private func didUpdateUserLocation() {
         guard let delegate = delegate else { return }
         delegate.didUpdateUserLocation()
-        print("updateUserLocation()")
+    }
+    
+    private func openSetting() {
+        guard let delegate = delegate else { return }
+        delegate.openSetting()
+
+    }
+}
+
+protocol LocationManagerDelegate: AnyObject, UIViewController {
+    func didUpdateUserLocation()
+}
+
+extension LocationManagerDelegate {
+    // TODO: - 임시로 설정한 message 변경
+    func openSetting() {
+        let alert = UIAlertController(title: "위치 서비스 사용", message: "위치서비스를 사용할 수 없습니다. 기기의 설정 > SpaceOver > 위치에서 위치 서비스를 켜주세요", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "설정으로 이동", style: .default, handler: { _ in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.open(url)
+            }
+        })
+        let destructiveAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+        
+        alert.addAction(destructiveAction)
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
     }
 }
