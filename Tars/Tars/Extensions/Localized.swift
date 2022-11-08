86e94c228434c9957d04a74e085d15fbc7eda0be
Seik oh <seioh22@pos.idserve.net>
2022-11-08 19:38:58 +0900

    [FEAT] add Localized string enum

--- Tars/Tars/Extensions/Localized.swift
+++ Tars/Tars/Extensions/Localized.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct LocalizableStrings {
+    static let jupiter  = NSLocalizedString("목성", comment: "")
+    static let jupiterContent = NSLocalizedString("목성은 태양계의 다섯번째 행성이자 가장 큰 행성이다. 태양의 질량의 1,000분의 1배에 달하는 거대행성으로, 태양계에 있는 다른 모든 행성들을 합한 질량의 약 2.5배에 이른다", comment: "")
+}
+
+enum PlanetStrings: String {
+    case jupiter, jupiterContent
+
+    var localizedKey: String {
+        switch self {
+        case .jupiter:
+            return LocalizableStrings.jupiter
+        case .jupiterContent:
+            return LocalizableStrings.jupiterContent
+        }
+    }
+}
