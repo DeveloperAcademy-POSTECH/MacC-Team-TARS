722a06dfa003fa3eb89a4ebe5340f570e0582570
Seik oh <seioh22@pos.idserve.net>
2022-11-15 17:26:12 +0900

    [FEAT] OnboradingViewController (#45)
    Added for the view related to LaunchScreenVC

---
+++ Tars/Tars/ViewController/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  Tars
+//
+//  Created by Seik Oh on 15/11/2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
