e603bf1d8d80422d9e33a80e654e9d86c4c8a842
lena <lenaminn@gmail.com>
2022-11-17 10:19:08 +0900

    [CHORE] edit SceneDelegate

--- Tars/Tars/SceneDelegate.swift
+++ Tars/Tars/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: UniverseViewController())
+        window?.rootViewController = UINavigationController(rootViewController: UniverseSearchViewController())
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
