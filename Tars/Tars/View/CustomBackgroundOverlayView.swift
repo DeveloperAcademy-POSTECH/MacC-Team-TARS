d7933dd7f1de80dac13e762f1e0927a1f7a475a6
Seik oh <seioh22@pos.idserve.net>
2022-11-19 23:36:37 +0900

    [ADD] add CustomBackgroundOverlayView (#54)
    for onboarding background

---
+++ Tars/Tars/View/CustomBackgroundOverlayView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomBackgroundOverlayView.swift
+//  Tars
+//
+//  Created by Seik Oh on 19/11/2022.
+//
+
+import UIKit
+
+class CustomBackgroundOverlayView: UIView {
+    
+    lazy var coachingOnboardingBackground: UIView = {
+        let background = UIView()
+        background.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        background.translatesAutoresizingMaskIntoConstraints = false
+        return background
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configureBackgroundOverlay()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureBackgroundOverlay() {
+        
+        [coachingOnboardingBackground].forEach { addSubview($0) }
+        
+        coachingOnboardingBackground.anchor(width: 100000, height: 100000)
+    }
+}
