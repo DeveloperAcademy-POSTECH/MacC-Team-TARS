42254ebd0247b5d69841f98db0d7f207bc5dbf7c
Ayden <kaitaro77@naver.com>
2022-11-24 00:13:14 +0900

    [FEAT] Custom Planet InfoView 생성, UI 구현

--- Tars/Tars/View/CustomPlanetInfoView.swift
+++ Tars/Tars/View/CustomPlanetInfoView.swift
@@ -8,5 +8,54 @@
 import UIKit
 
 class CustomPlanetInfoView: UIView {
-
+    
+    lazy var chapter: UILabel = {
+        let chapter = UILabel()
+        chapter.text = "Chapter 1"
+        chapter.font = .preferredFont(forTextStyle: .title2)
+        chapter.textColor = .white
+        chapter.adjustsFontForContentSizeCategory = true
+        return chapter
+    }()
+    
+    lazy var planetInfoTitle: UILabel = {
+        let planetInfoTitle = UILabel()
+        planetInfoTitle.text = planetContentsList[0].planetInfoFirstTitle
+        if let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1).withSymbolicTraits(.traitBold) {
+            planetInfoTitle.font = .init(descriptor: descriptor, size: 0)
+        }
+        planetInfoTitle.textColor = .white
+        planetInfoTitle.textAlignment = .left
+        planetInfoTitle.adjustsFontForContentSizeCategory = true
+       return planetInfoTitle
+    }()
+    
+    lazy var planetInfoContents: UILabel = {
+        let planetInfoContents = UILabel()
+        planetInfoContents.text = planetContentsList[0].planetInfoFirstContents
+        planetInfoContents.font = .preferredFont(forTextStyle: .title2)
+        planetInfoContents.textColor = .white
+        planetInfoContents.textAlignment = .left
+        planetInfoContents.numberOfLines = 0
+        planetInfoContents.setLineSpacing(spacing: 6)
+        planetInfoContents.adjustsFontForContentSizeCategory = true
+       return planetInfoContents
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configurePlanetInfoContents()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configurePlanetInfoContents() {
+        [chapter, planetInfoTitle, planetInfoContents].forEach { addSubview($0) }
+        
+        chapter.anchor(top: self.topAnchor, leading: self.leadingAnchor, paddingLeading: screenWidth / 12.18)
+        planetInfoTitle.anchor(top: chapter.bottomAnchor, leading: self.leadingAnchor, paddingTop: screenHeight / 140.6, paddingLeading: screenWidth / 12.18)
+        planetInfoContents.anchor(top: planetInfoTitle.bottomAnchor, leading: self.leadingAnchor, paddingTop: screenHeight / 52.75, paddingLeading: screenWidth / 12.18, width: screenWidth / 1.19)
+    }
 }
--- Tars/Tars/ViewController/InfoViewController.swift
+++ Tars/Tars/ViewController/InfoViewController.swift
@@ -10,6 +10,7 @@ import SceneKit.ModelIO
 
 class InfoViewController: UIViewController {
     public var planet: Planet = Planet(planetKoreanName: "", planetEnglishName: "", planetImage: UIImage(named: ""))
+    private var planetContentsView = CustomPlanetInfoView()
     
     lazy var sceneView: SCNView = {
         let sceneView = SCNView()
@@ -54,35 +55,35 @@ class InfoViewController: UIViewController {
         return sceneView
     }()
     
-    lazy var planetName: UILabel = {
-        let planetName = UILabel()
-        planetName.text = planet.planetKoreanName
-        planetName.font = .preferredFont(forTextStyle: .largeTitle)
-        if let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1).withSymbolicTraits(.traitBold) {
-            planetName.font = .init(descriptor: descriptor, size: 0)
-        }
-        planetName.textColor = .white
-        planetName.adjustsFontForContentSizeCategory = true
-        return planetName
-    }()
+//    lazy var planetName: UILabel = {
+//        let planetName = UILabel()
+//        planetName.text = planet.planetKoreanName
+//        planetName.font = .preferredFont(forTextStyle: .largeTitle)
+//        if let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1).withSymbolicTraits(.traitBold) {
+//            planetName.font = .init(descriptor: descriptor, size: 0)
+//        }
+//        planetName.textColor = .white
+//        planetName.adjustsFontForContentSizeCategory = true
+//        return planetName
+//    }()
     
-    lazy var planetInfo: UILabel = {
-        let planetInfo = UILabel()
-        // TODO: content text 수정
-        planetInfo.text = PlanetStrings.jupiterContent.localizedKey
-        planetInfo.font = .preferredFont(forTextStyle: .title3)
-        planetInfo.setLineSpacing(spacing: 6)
-        planetInfo.textColor = .white
-        planetInfo.adjustsFontForContentSizeCategory = true
-        planetInfo.numberOfLines = 0
-        return planetInfo
-    }()
+//    lazy var planetInfo: UILabel = {
+//        let planetInfo = UILabel()
+//        // TODO: content text 수정
+//        planetInfo.text = PlanetStrings.jupiterContent.localizedKey
+//        planetInfo.font = .preferredFont(forTextStyle: .title3)
+//        planetInfo.setLineSpacing(spacing: 6)
+//        planetInfo.textColor = .white
+//        planetInfo.adjustsFontForContentSizeCategory = true
+//        planetInfo.numberOfLines = 0
+//        return planetInfo
+//    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black
-        [sceneView, planetName, planetInfo].forEach { view.addSubview($0) }
+        [sceneView, planetContentsView].forEach { view.addSubview($0) }
         configureConstraints()
     }
 
@@ -90,10 +91,6 @@ class InfoViewController: UIViewController {
         sceneView.centerX(inView: view)
         sceneView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: screenHeight / 21.6, width: screenWidth, height: screenWidth / 1.56)
         
-        planetName.centerX(inView: view)
-        planetName.anchor(top: sceneView.bottomAnchor, paddingTop: screenHeight / 21.6)
-        
-        planetInfo.centerX(inView: view)
-        planetInfo.anchor(top: planetName.bottomAnchor, leading: view.leadingAnchor, trailing: view.trailingAnchor, paddingTop: screenWidth / 26.3, paddingLeading: screenWidth / 9.75, paddingTrailing: screenWidth / 9.75)
+        planetContentsView.anchor(top: sceneView.bottomAnchor, paddingTop: screenHeight / 21.1, paddingLeading: screenWidth / 12.18)
     }
 }
