9d23117633c69c1d99dbf6f03585a63fc2bb029e
Ayden <kaitaro77@naver.com>
2022-11-23 16:46:09 +0900

    [ADD] Planet Model Init

--- Tars/Tars/Model/Planet.swift
+++ Tars/Tars/Model/Planet.swift
@@ -11,16 +11,31 @@ struct Planet {
     var planetKoreanName: String
     var planetEnglishName: String
     var planetImage: UIImage?
+    var planetDescription: String?
     
     init(planetKoreanName: String, planetImage: UIImage?) {
         self.planetKoreanName = planetKoreanName
         self.planetEnglishName = ""
         self.planetImage = planetImage
     }
-    
+
     init(planetKoreanName: String, planetEnglishName: String, planetImage: UIImage?) {
         self.planetKoreanName = planetKoreanName
         self.planetEnglishName = planetEnglishName
         self.planetImage = planetImage
+    }
+
+    init(planetKoreanName: String, planetEnglishName: String, planetImage: UIImage?, planetDescription: String?) {
+        self.planetKoreanName = planetKoreanName
+        self.planetEnglishName = planetEnglishName
+        self.planetImage = planetImage
+        self.planetDescription = planetDescription
+    }
+    
+    init() {
+        self.planetKoreanName = ""
+        self.planetEnglishName = ""
+        self.planetImage = nil
+        self.planetDescription = ""
     }
 }
