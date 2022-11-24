14446b812dede79ae922f977c4e2bc241d4bc0a0
Ayden <kaitaro77@naver.com>
2022-11-24 11:30:17 +0900

    [FEAT] Contents View 컨텐츠 넣음

--- Tars/Tars/View/CustomPlanetInfoView.swift
+++ Tars/Tars/View/CustomPlanetInfoView.swift
@@ -11,7 +11,6 @@ class CustomPlanetInfoView: UIView {
     
     lazy var chapter: UILabel = {
         let chapter = UILabel()
-        chapter.text = "Chapter 1"
         chapter.font = .preferredFont(forTextStyle: .title2)
         chapter.textColor = .white
         chapter.adjustsFontForContentSizeCategory = true
@@ -20,7 +19,7 @@ class CustomPlanetInfoView: UIView {
     
     lazy var planetInfoTitle: UILabel = {
         let planetInfoTitle = UILabel()
-        planetInfoTitle.text = planetContentsList[0].planetInfoFirstTitle
+        planetInfoTitle.text = ""
         if let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1).withSymbolicTraits(.traitBold) {
             planetInfoTitle.font = .init(descriptor: descriptor, size: 0)
         }
@@ -30,20 +29,41 @@ class CustomPlanetInfoView: UIView {
        return planetInfoTitle
     }()
     
+//    lazy var planetInfoContents: UILabel = {
+//        let planetInfoContents = UILabel()
+//        planetInfoContents.text = ""
+//        planetInfoContents.font = .preferredFont(forTextStyle: .title2)
+//        planetInfoContents.textColor = .white
+//        planetInfoContents.textAlignment = .left
+//        planetInfoContents.numberOfLines = 0
+//        planetInfoContents.setLineSpacing(spacing: 6)
+//        planetInfoContents.adjustsFontForContentSizeCategory = true
+//       return planetInfoContents
+//    }()
+    
     lazy var planetInfoContents: UILabel = {
-        let planetInfoContents = UILabel()
-        planetInfoContents.text = planetContentsList[0].planetInfoFirstContents
-        planetInfoContents.font = .preferredFont(forTextStyle: .title2)
-        planetInfoContents.textColor = .white
-        planetInfoContents.textAlignment = .left
-        planetInfoContents.numberOfLines = 0
-        planetInfoContents.setLineSpacing(spacing: 6)
-        planetInfoContents.adjustsFontForContentSizeCategory = true
-       return planetInfoContents
+        let label = UILabel()
+        let attributedString = NSMutableAttributedString(string: "")
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.numberOfLines = 0
+        label.attributedText = attributedString
+        label.textAlignment = .center
+        paragraphStyle.lineSpacing = 10
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+        label.textColor = .white
+        label.adjustsFontForContentSizeCategory = true
+        return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        
+//        for i in planetContentsList {
+//            planetInfoTitle.text = i.planetTitle1
+//            planetInfoContents.text = i.planetContents1
+//        }
         configurePlanetInfoContents()
     }
     
@@ -54,8 +74,22 @@ class CustomPlanetInfoView: UIView {
     private func configurePlanetInfoContents() {
         [chapter, planetInfoTitle, planetInfoContents].forEach { addSubview($0) }
         
-        chapter.anchor(top: self.topAnchor, leading: self.leadingAnchor, paddingLeading: screenWidth / 12.18)
-        planetInfoTitle.anchor(top: chapter.bottomAnchor, leading: self.leadingAnchor, paddingTop: screenHeight / 140.6, paddingLeading: screenWidth / 12.18)
-        planetInfoContents.anchor(top: planetInfoTitle.bottomAnchor, leading: self.leadingAnchor, paddingTop: screenHeight / 52.75, paddingLeading: screenWidth / 12.18, width: screenWidth / 1.19)
+        chapter.anchor(top: self.topAnchor,
+                       leading: self.leadingAnchor,
+                       trailing: self.trailingAnchor,
+                       paddingLeading: screenWidth / 12.18)
+        planetInfoTitle.anchor(top: chapter.bottomAnchor,
+                               leading: self.leadingAnchor,
+                               trailing: self.trailingAnchor,
+                               paddingTop: screenHeight / 140.6,
+                               paddingLeading: screenWidth / 12.18)
+        planetInfoContents.anchor(top: planetInfoTitle.bottomAnchor,
+                                  leading: self.leadingAnchor,
+                                  bottom: self.bottomAnchor,
+                                  trailing: self.trailingAnchor,
+                                  paddingTop: screenHeight / 52.75,
+                                  paddingLeading: screenWidth / 12.18,
+                                  paddingBottom: screenHeight / 21.1,
+                                  width: screenWidth / 1.19)
     }
 }
