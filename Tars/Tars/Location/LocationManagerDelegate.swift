b89bb849fed474814954c4ea1a6768aa4607a2ba
lena <lenaminn@gmail.com>
2022-11-28 20:07:13 +0900

    [CHORE] 권한설정 로컬라이징 (#90)

--- Tars/Tars/Location/LocationManagerDelegate.swift
+++ Tars/Tars/Location/LocationManagerDelegate.swift
@@ -14,14 +14,14 @@ protocol LocationManagerDelegate: AnyObject, UIViewController {
 extension LocationManagerDelegate {
     // TODO: - 임시로 설정한 message 변경
     func openSetting() {
-        let alert = UIAlertController(title: "위치 서비스 사용", message: "위치서비스를 사용할 수 없습니다. 기기의 설정 > SpaceOver > 위치에서 위치 서비스를 켜주세요", preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "설정으로 이동", style: .default, handler: { _ in
+        let alert = UIAlertController(title: NSLocalizedString("위치 서비스 사용", comment: "") , message:  NSLocalizedString("위치서비스를 사용할 수 없습니다. 기기의 설정 > SpaceOver > 위치에서 위치 서비스를 켜주세요", comment: ""), preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: NSLocalizedString("설정으로 이동", comment: ""), style: .default, handler: { _ in
             guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
             DispatchQueue.main.async {
                 UIApplication.shared.open(url)
             }
         })
-        let destructiveAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+        let destructiveAction = UIAlertAction(title: NSLocalizedString("취소", comment: ""), style: .destructive, handler: nil)
         
         alert.addAction(destructiveAction)
         alert.addAction(defaultAction)
