d48a764e285b15821b486dc70e02898374317900
lenamin <lenaminn@gmail.com>
2024-07-10 19:49:29 +0900

    [ADD] string catalog의 키 값들에 접근할 수 있도록 enum으로 구현 (#103)

---
+++ Tars/Tars/Global/Localization/LocalizableKeys.swift
@@ -0,0 +1,28 @@
+//
+//  localized.swift
+//  Tars
+//
+//  Created by Seik Oh on 06/11/2022.
+//
+
+import Foundation
+
+enum LocalizableKeys: String {
+    case sun, moon, mercury, venus, mars, jupiter, saturn, uranus, neptune
+    case airPodsInstructionstring, onboardingInstructionstring, onboardingInstructionTitle
+    case collectionViewTitle, collectionViewContent
+    case exploreUniverseNavigationTitle, searchingNavigationTitle
+    case directionUp, directionUpRight, directionRight, directionDownRight, directionDown, directionDownLeft, directionLeft, directionUpLeft
+    case image
+    case chapterOneHint, chapterTwoHint, chapterThreeHint
+    case sunChapterOneTitle, sunChapterOneContent, sunChapterTwoTitle, sunChapterTwoContent, sunChapterThreeTitle, sunChapterThreeContent
+    case moonChapterOneTitle, moonChapterOneContent, moonChapterTwoTitle, moonChapterTwoContent, moonChapterThreeTitle, moonChapterThreeContent
+    case mercuryChapterOneTitle, mercuryChapterOneContent, mercuryChapterTwoTitle, mercuryChapterTwoContent, mercuryChapterThreeTitle, mercuryChapterThreeContent
+    case venusChapterOneTitle, venusChapterOneContent, venusChapterTwoTitle, venusChapterTwoContent, venusChapterThreeTitle, venusChapterThreeContent
+    case marsChapterOneTitle, marsChapterOneContent, marsChapterTwoTitle, marsChapterTwoContent, marsChapterThreeTitle, marsChapterThreeContent
+    case jupiterChapterOneTitle, jupiterChapterOneContent, jupiterChapterTwoTitle, jupiterChapterTwoContent, jupiterChapterThreeTitle, jupiterChapterThreeContent
+    case saturnChapterOneTitle, saturnChapterOneContent, saturnChapterTwoTitle, saturnChapterTwoContent, saturnChapterThreeTitle, saturnChapterThreeContent
+    case uranusChapterOneTitle, uranusChapterOneContent, uranusChapterTwoTitle, uranusChapterTwoContent, uranusChapterThreeTitle, uranusChapterThreeContent
+    case neptuneChapterOneTitle, neptuneChapterOneContent, neptuneChapterTwoTitle, neptuneChapterTwoContent, neptuneChapterThreeTitle, neptuneChapterThreeContent
+    
+}
