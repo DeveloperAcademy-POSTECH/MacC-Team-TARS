59dd40fa9df506ade33436dbfc7a54d609c9cb27
lenamin <lenaminn@gmail.com>
2024-07-14 20:52:12 +0900

    [ADD] init 메서드 추가 (#106)

--- Tars/Tars/Global/Localization/LocalizableKeys.swift
+++ Tars/Tars/Global/Localization/LocalizableKeys.swift
@@ -1,8 +1,8 @@
 //
-//  localized.swift
+//  LocalizableKeys.swift
 //  Tars
 //
-//  Created by Seik Oh on 06/11/2022.
+//  Created by Lena on 12/07/2024.
 //
 
 import Foundation
@@ -11,10 +11,15 @@ enum LocalizableKeys: String {
     case sun, moon, mercury, venus, mars, jupiter, saturn, uranus, neptune
     case airPodsInstructionstring, onboardingInstructionstring, onboardingInstructionTitle
     case collectionViewTitle, collectionViewContent
+    
     case exploreUniverseNavigationTitle, searchingNavigationTitle
     case directionUp, directionUpRight, directionRight, directionDownRight, directionDown, directionDownLeft, directionLeft, directionUpLeft
+    case locationUsageMessage, locationAuthRequest, defaultAction, cancel
+    
     case image
+    
     case chapterOneHint, chapterTwoHint, chapterThreeHint
+    
     case sunChapterOneTitle, sunChapterOneContent, sunChapterTwoTitle, sunChapterTwoContent, sunChapterThreeTitle, sunChapterThreeContent
     case moonChapterOneTitle, moonChapterOneContent, moonChapterTwoTitle, moonChapterTwoContent, moonChapterThreeTitle, moonChapterThreeContent
     case mercuryChapterOneTitle, mercuryChapterOneContent, mercuryChapterTwoTitle, mercuryChapterTwoContent, mercuryChapterThreeTitle, mercuryChapterThreeContent
@@ -25,10 +30,18 @@ enum LocalizableKeys: String {
     case uranusChapterOneTitle, uranusChapterOneContent, uranusChapterTwoTitle, uranusChapterTwoContent, uranusChapterThreeTitle, uranusChapterThreeContent
     case neptuneChapterOneTitle, neptuneChapterOneContent, neptuneChapterTwoTitle, neptuneChapterTwoContent, neptuneChapterThreeTitle, neptuneChapterThreeContent
     
+    /// 문자 형태의 string을 LocalizableKeys의 열거형으로 변환
+    init?(from string: String) {
+        self.init(rawValue: string)
+    }
+    
+    /// LocalizableKeys의 값을  시스템 언어로 localize
     var localized: String {
         return self.rawValue.localized()
     }
     
+    /// 원하는 언어로 localize
+    /// - Parameter language: .english / .korean 중 선택
     func localized(for language: Language) -> String {
         return self.rawValue.localized(for: language)
     }
